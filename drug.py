a176bd465ca3d252e4232ad5d828cc4253731c23
James Barney <jamesbarney71@gmail.com>
2017-11-21 16:07:34 -0500

    finalizing webscraper

--- drug.py
+++ drug.py
@@ -1,11 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-class Drug(object):
+class Drug:
 	def __init__(self, name, url):
 		self.name = name
 		self.url = url
-		self.info = self.get_info()
+		self.overview_page = self.get_overview_page()
+		self.overview_questions = {}
 
-	def get_info(self):
+		self.get_overview_questions()
+
+	def get_overview_page(self):
 		return BeautifulSoup(requests.get(self.url).content, 'lxml')
+
+	def get_overview_questions(self):
+		headers = self.overview_page.find_all("h2")
+		for head in headers:
+			self.overview_questions[head.string] = ""
+			nextNode = head
+			while True:
+				try: 
+					nextNode = nextNode.nextSibling
+					try:
+						tag_name = nextNode.name
+					except AttributeError:
+						tag_name = ""
+					try:
+						if tag_name == "p":
+							if nextNode.string is None:
+								self.overview_questions[head.string] += " " + str(nextNode.text).replace('\n', ' ').replace("\"", "")
+							elif nextNode is not None:
+								self.overview_questions[head.string] += " " + str(nextNode.string)
+						elif tag_name == "ul" or tag_name == "li":
+							self.overview_questions[head.string] += " " + str(nextNode.get_text(strip=True))
+						elif tag_name == "br":
+							print("br!")
+							self.overview_questions[head.string] += " " + str(nextNode.get_text(strip=True))
+						elif tag_name == "h2":
+							break
+					except TypeError:
+						continue
+				except AttributeError:
+					break
+
+if __name__ == '__main__':
+	abilify = Drug("Acetaminophen", "https://www.drugs.com/acetaminophen.html")
+	print(abilify.overview_questions["Acetaminophen dosing information"])
+
+	
--- mypharmacy.py
+++ mypharmacy.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 from drug import Drug
 
@@ -9,19 +10,30 @@ def get_popular():
 def main():
 	base_url = "https://www.drugs.com"
 	alpha_url = "/alpha"
-	alphabet = "a"
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	#alphabet = "a"
 	raw_all_drugs = ""
 	for letter in alphabet:
+		print(letter)
 		url = base_url+alpha_url+"/"+letter+"1.html"
 		# print(url)
 		page = BeautifulSoup(requests.get(url).content, "lxml")
-		raw_all_drugs = page.find_all("div", ["boxListPopular"])
-	raw_all_drugs = str(raw_all_drugs[0])
+		letter_drugs = page.find_all("div", ["boxListPopular"])
+		raw_all_drugs += str(letter_drugs[0])
 
 	all_drugs = {}
 	drug_parsed = BeautifulSoup(raw_all_drugs, "lxml")
 	for drug in drug_parsed.find_all("a", href=True):
 		all_drugs[drug.string] = Drug(drug.string, base_url+drug["href"])
+
+	
+
+	with open('all_drugs.json', 'w') as f:
+		for name, drug in all_drugs.items():
+			json.dump({name: drug.overview_questions}, f)
+			f.write('\n')
+
+
 
 
 
